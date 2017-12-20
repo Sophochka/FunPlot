030726cd9ff1ebf93afaecaebaa2ddc5dceb4ad5
Sophia <sophochka.2002@gmail.com>
2017-12-20 23:55:49 +0200

    First version

---
+++ fplot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""My first python utility"""
+
+import math
+import inspect
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# ---------------------------------------------
+# INPUT DATA
+# ---------------------------------------------
+# Type function to display after colon
+FUNCTION = lambda x: math.sin(x)
+
+# Type min X, max X and step
+MIN_X = -10.0
+MAX_X = 10.0
+D_X = 0.1
+# --------------------------------------------- 
+
+# --------------------------------------------- 
+# APPEARANCE
+# --------------------------------------------- 
+CHAR = '.'
+matplotlib.rcParams['axes.unicode_minus'] = False
+# --------------------------------------------- 
+
+
+def func_source(func):
+    """Return function source string"""
+    func_str = str(inspect.getsourcelines(func)[0])
+    func_str = func_str.strip("['\\n']").split(" = ")[1].split(":")[1].strip()
+    return func_str
+
+
+X = np.arange(MIN_X, MAX_X, D_X)
+Y = list(map(FUNCTION, X))
+
+fig, ax = plt.subplots()
+ax.plot(X, Y, CHAR)
+ax.set_title('Function: ' + func_source(FUNCTION))
+plt.show()
