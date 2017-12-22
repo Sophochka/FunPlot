de18389ba4d440a4f91132fc176f5bb509520b05
Sophia <sophochka.2002@gmail.com>
2017-12-23 00:00:11 +0200

    Allow to display a few functions

--- funplot.py
+++ funplot.py
@@ -8,31 +8,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # ---------------------------------------------
-# INPUT DATA
-# ---------------------------------------------
-# Type function to display after colon
-FUNCTION = lambda x: math.sin(x)
-
 # Type min X, max X and step
 MIN_X = -10.0
 MAX_X = 10.0
 D_X = 0.1
 # ---------------------------------------------
-CHAR = '.'
-COLOR = 'r'
+DATA = (
+    # Type functions to display after colon, point char, color:
+    (lambda x: math.sin(x), '.', 'r'),
+    (lambda x: math.cos(x), '*', 'b')
+)
 # ---------------------------------------------
 
+# Utils
 def func_source(func):
     """Return function source string"""
     func_str = str(inspect.getsourcelines(func)[0])
-    func_str = func_str.strip("['\\n']").split(" = ")[1].split(":")[1].strip()
-    return func_str
+    func_str = func_str[func_str.find(":")+1:func_str.find(",")].strip()
+    #func_str = func_str.strip("['\\n']").split(" = ")[1].split(":")[1].strip()
+    return "y = " + func_str
 
+# Init plot and captions
+FIG, AX = plt.subplots()
+AX.set_title('Fun Plot')
+AX.set_xlabel('X')
+AX.set_ylabel('Y')
 
+# Draw data
 X = np.arange(MIN_X, MAX_X, D_X)
-Y = list(map(FUNCTION, X))
+for f in DATA:
+    AX.plot(X, list(map(f[0], X)), f[1], color=f[2])
 
-fig, ax = plt.subplots()
-ax.plot(X, Y, CHAR, color=COLOR)
-ax.set_title('Function: ' + func_source(FUNCTION))
+# Add legend and show  plot
+AX.legend(list(map(func_source, (f[0] for f in DATA))))
 plt.show()
+
